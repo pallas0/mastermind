8fca8c49055ae584c4f2eab03c22a92b87eef01f
Amelia Risner <amelia.risner0@gmail.com>
2023-11-02 18:39:38 -0700

    cors stuff added, still not working

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000/"}})
 numbers = []
 
 @app.route('/generate', methods=['GET'])
