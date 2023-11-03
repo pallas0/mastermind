f3cc87b8864f5530a400c16d77bf503e1348dd3f
Amelia Risner <amelia.risner0@gmail.com>
2023-11-02 21:01:23 -0700

    game initilization switched to useEffect

--- mastermind-frontend/src/App.js
+++ mastermind-frontend/src/App.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './App.css';
 
 function App() {
-  const [numbers, setNumbers] = useState([]);
-  const [guess, setGuess] = useState([]);
-  const [history, setHistory] = useState([]);
+  const [numbers, setNumbers] = useState(['hi!']);
+  const [guesses, setGuesses] = useState([]);
+  const [feedback, setFeedback] = useState([]);
+  const [attempts, setAttempts] = useState(10);
 
-  const generateNumbers = async() => {
-    const response = await axios.get('http://127.0.0.1:5000/generate')
-    console.log(response);
-  }
+  useEffect(() => {
+    axios.get('http://localhost:5000/generate')
+    .then(response => {
+      setNumbers(response.data.numbers);
+      setAttempts(response.data.attempts);
+    })
+  }, [])
 
   return (
     <div className="App">
-      <button onClick={generateNumbers}>Start Game</button>
       <header>{numbers}</header>
     </div>
   );
