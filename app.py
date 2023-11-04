42755f953ef055baf3337559696d8443dd4b4def
Amelia Risner <amelia.risner0@gmail.com>
2023-11-04 15:00:43 -0700

    update history added, check_gameover started (might wait until game flow is better established)

--- app.py
+++ app.py
@@ -6,21 +6,20 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 class Game:
-  def __init__(self, number, guesses=[], feedback=[], game_over=False, number_length=4, attempts=10):
+  def __init__(self, number, guesses=[], feedback=[], player_won=[], number_length=4, attempts=10):
       self.number = number
       self.guesses = guesses
       self.feedback = feedback
-      self.game_over = game_over
+      self.player_won = player_won
       self.number_length = number_length
       self.attempts = attempts
 
-  def provide_feedback(self, guess):
+  def process_guess(self, guess):
+    self.attempts -= 1
     correct_numbers, correct_locations = 0,0
     number_dict = {}
-    
     for elem in self.number:
       number_dict[elem] = number_dict.get(elem,0) + 1
-
     for i in range(len(self.number)):
       if guess[i] == self.number[i]:
         correct_locations += 1
@@ -29,9 +28,23 @@ class Game:
         if number_dict[guess[i]] == 0:
           number_dict.pop(guess[i])
         correct_numbers += 1
-        
     feedback = f"{correct_numbers} right numbers, {correct_locations} in the right location"
+    self.update_history(guess, feedback)
+    #self.check_gameover()
     return feedback
+
+  def update_history(self, guess, feedback):
+    self.guesses.append(guess)
+    self.feedback.append(feedback)
+
+  #ok this flow we can iron out later
+  # def check_gameover(self):
+  #   if self.guesses[-1] == self.number :
+  #     self.player_won.append(True)
+  #     return True
+  #   elif self.attempts == 0:
+  #     self.player_won.append(False)
+  #   return False
     
 
 numbers = []
