b9d335305269d4177cff3ef165eefd8775635420
Amelia Risner <amelia.risner0@gmail.com>
2023-11-03 20:47:35 -0700

    provide feedback method added

--- app.py
+++ app.py
@@ -6,14 +6,32 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 class Game:
-    def __init__(self, number, guesses, feedback, game_over=False, number_length=4, attempts=10):
-        self.number = number
-        self.guesses = []
-        self.feedback = []
-        self.game_over = game_over
-        self.number_length = number_length
-        self.attempts = attempts
+  def __init__(self, number, guesses=[], feedback=[], game_over=False, number_length=4, attempts=10):
+      self.number = number
+      self.guesses = guesses
+      self.feedback = feedback
+      self.game_over = game_over
+      self.number_length = number_length
+      self.attempts = attempts
 
+  def provide_feedback(self, guess):
+    correct_numbers, correct_locations = 0,0
+    number_dict = {}
+    
+    for elem in self.number:
+      number_dict[elem] = number_dict.get(elem,0) + 1
+
+    for i in range(len(self.number)):
+      if guess[i] == self.number[i]:
+        correct_locations += 1
+      if guess[i] in number_dict:
+        number_dict[guess[i]] -= 1
+        if number_dict[guess[i]] == 0:
+          number_dict.pop(guess[i])
+        correct_numbers += 1
+        
+    feedback = f"{correct_numbers} right numbers, {correct_locations} in the right location"
+    return feedback
     
 
 numbers = []
