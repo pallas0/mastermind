2e9c9b1facb2583440c2676348ba1aad9f5630f2
Amelia Risner <amelia.risner0@gmail.com>
2023-11-04 19:03:55 -0700

    ok, basic start button created, need to figure out how to get whole game object jsonified and sent over

--- mastermind-frontend/src/App.js
+++ mastermind-frontend/src/App.js
@@ -4,22 +4,21 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [numbers, setNumbers] = useState(['hi!']);
   const [guesses, setGuesses] = useState([]);
   const [feedback, setFeedback] = useState([]);
   const [attempts, setAttempts] = useState(10);
 
-  useEffect(() => {
+
+  const startGame = async() => {
     axios.get('http://127.0.0.1:5000/generate')
     .then(response => {
-      setNumbers(response.data.numbers);
-      setAttempts(response.data.attempts);
+      console.log(response)
     })
-  }, [])
+  }
 
   return (
     <div className="App">
-      <header>{numbers}</header>
+      <button onClick={startGame}>Start Game</button>
     </div>
   );
 }
