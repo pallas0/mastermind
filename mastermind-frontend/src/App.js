a25524b8c5534b00461584bff4c97b517387658c
Amelia Risner <amelia.risner0@gmail.com>
2023-11-06 18:54:39 -0800

    text field and guess submission added

--- mastermind-frontend/src/App.js
+++ mastermind-frontend/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [guess, setGuess] = useState("")
   const [guesses, setGuesses] = useState([]);
   const [feedback, setFeedback] = useState([]);
   const [attempts, setAttempts] = useState(10);
@@ -12,13 +13,31 @@ function App() {
   const startGame = async() => {
     axios.get('http://127.0.0.1:5000/generate')
     .then(response => {
-      console.log(response)
+      setGuess("")
+      setAttempts(response.data.attempts);
+    })
+  }
+
+  const submitGuess = () => {
+    axios.post('http://127.0.0.1:5000/compare_guess', {guess})
+    .then(response => {
+      console.log(response.data);
+      setGuesses([...guesses, guess])
+      setGuess("")
     })
   }
 
   return (
     <div className="App">
       <button onClick={startGame}>Start Game</button>
+      <input
+      type="text"
+      placeholder="Enter your guess"
+      value={guess}
+      onChange={(e) => setGuess(e.target.value)}
+      />
+      <button onClick={submitGuess}>Submit Guess</button>
+      <p>Attempts: {attempts}</p>
     </div>
   );
 }
