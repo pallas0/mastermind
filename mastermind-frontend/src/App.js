33b5fead5c7628186496dce51094cef0b7949b04
Amelia Risner <amelia.risner0@gmail.com>
2023-11-06 19:15:55 -0800

    frontend now lists feedback and guess history

--- mastermind-frontend/src/App.js
+++ mastermind-frontend/src/App.js
@@ -14,6 +14,8 @@ function App() {
     axios.get('http://127.0.0.1:5000/generate')
     .then(response => {
       setGuess("")
+      setGuesses([])
+      setFeedback([])
       setAttempts(response.data.attempts);
     })
   }
@@ -22,7 +24,9 @@ function App() {
     axios.post('http://127.0.0.1:5000/compare_guess', {guess})
     .then(response => {
       console.log(response.data);
+      setAttempts(attempts-1)
       setGuesses([...guesses, guess])
+      setFeedback([...feedback, response.data.feedback])
       setGuess("")
     })
   }
@@ -31,13 +35,31 @@ function App() {
     <div className="App">
       <button onClick={startGame}>Start Game</button>
       <input
-      type="text"
-      placeholder="Enter your guess"
-      value={guess}
-      onChange={(e) => setGuess(e.target.value)}
+        type="text"
+        placeholder="Enter your guess"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value)}
       />
       <button onClick={submitGuess}>Submit Guess</button>
       <p>Attempts: {attempts}</p>
+      <div className="guess-feedback">
+        <div className="guesses">
+          <h3>Guesses</h3>
+          <ul>
+            {guesses.map((guess, index) => (
+              <li key={index}>{guess}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="feedback">
+          <h3>Feedback</h3>
+          <ul>
+            {feedback.map((fb, index) => (
+              <li key={index}>{fb}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
